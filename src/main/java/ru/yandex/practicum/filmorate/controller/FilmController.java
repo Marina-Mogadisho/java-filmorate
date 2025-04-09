cbd56312ecd59f9a69fe11bc46bf915e4b5c3ffa
marina <shadrinam2012@yandex.ru>
2025-04-09 08:13:00 +0300

    feat - новый функционал ФЗ 10 спринта (правки по замечанию ревьюера. Итерация 1)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.util.UtilData;
 
 import java.time.LocalDate;
 import java.util.*;
